6551c602a8d01e4fe1e9251dcf7a5f710d37e9f9
miloszwasacz <wasacz.dev@gmail.com>
2025-03-02 23:10:00 +0000

    Add exit code logging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-use cpu_sim::components::cpu::Cpu;
+use cpu_sim::components::cpu::{Cpu, CpuRun};
 use cpu_sim::components::memory::Memory;
 use cpu_sim::os::loader::Loader;
 use std::cell::RefCell;
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     const FILE: &str = "test/res/bin/bubble_sort";
 
     let mem = RefCell::new(Memory::new());
     let mut cpu = Cpu::new(&mem);
     if let Err(err) = Loader.load(FILE, &mut mem.borrow_mut(), &mut cpu) {
         eprintln!("{}", err);
-        std::process::exit(2);
+        return ExitCode::FAILURE;
     }
 
-    if let Err(err) = cpu.run() {
-        eprintln!("{}", err);
-        std::process::exit(1);
+    loop {
+        match cpu.run() {
+            Ok(CpuRun::Exit(exit_code)) => {
+                eprintln!("process exited with code: {}", exit_code);
+                return ExitCode::SUCCESS;
+            }
+            Ok(CpuRun::Break) => {
+                eprintln!("process break");
+                continue;
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+                return ExitCode::FAILURE;
+            }
+        }
     }
 }
